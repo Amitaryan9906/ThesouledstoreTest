bd082557c2b296d4629b14169f162c662d51d754
Amit-Das <damit5221@gmail.com>
2023-04-22 23:53:07 +0530

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,10 @@ module.exports = defineConfig({
  
   e2e: {
     specPattern: 'cypress/integration/ActualTestCases/*.js',
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    
+      require('cypress-mochawesome-reporter/plugin')(on);
     }
   }
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-xpath';
-
+import 'cypress-mochawesome-reporter/register';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
